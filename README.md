1bc6bd82c04f83e135cc1d3423640715d93e5565
0rdinary-J0e <joseph.n.sweeney@gmail.com>
2019-06-06 16:29:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-1xafie
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-1xafie)
---
+++ src/app/hero.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class HeroService {
+
+  constructor(private messsageService: MessageService) { }
+
+
+  getHeroes(): Observable<Hero[]> {
+  this.messsageService.add('HeroService: fetched heroes');
+  return of(HEROES);
+}
+
+ ngOnInit() {
+  this.getHeroes();
+}
+
+
+}
